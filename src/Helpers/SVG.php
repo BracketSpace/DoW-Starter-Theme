d486502fa8d259875a2f8c90729b8c4a1a7b9969
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-01-19 16:00:09 +0100

    add SVG helper

---
+++ src/Helpers/SVG.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package DoW Starter Theme
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace DoWStarterTheme\Helpers;
+
+use DoWStarterTheme\Core\Theme;
+
+/**
+ * SVG helper class
+ */
+class SVG
+{
+    /**
+     * Returns SVG file content
+     *
+     * @param  string $filename File name.
+     * @return string|null
+     */
+    public static function get($filename): ?string
+    {
+        if (substr($filename, -4) !== '.svg') {
+            $filename .= '.svg';
+        }
+
+        $fs = Theme::getFs();
+        $path = "assets/dist/images/{$filename}";
+
+        if ($fs->exists($path)) {
+            $content = $fs->get_contents($path);
+
+            if (is_string($content)) {
+                return static::removeDoctype($content);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets SVG file from attachment
+     *
+     * @param  int $id File ID.
+     * @return string/void
+     */
+    public static function getAttachment(int $id): ?string
+    {
+        $filepath = get_attached_file($id);
+
+        if (!is_string($filepath) || !file_exists($filepath)) {
+            return null;
+        }
+
+        // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+        $content = file_get_contents($filepath);
+
+        if (! is_string($content)) {
+            return null;
+        }
+
+        return static::removeDoctype($content);
+    }
+
+    /**
+     * Echoes the SVG file content.
+     *
+     * @param  string $filename File name.
+     * @return void
+     */
+    public static function print($filename)
+    {
+        echo static::get($filename);
+    }
+
+    /**
+     * Echoes the SVG file from attachment
+     *
+     * @param  int $id File ID.
+     * @return void
+     */
+    public static function printAttachment(int $id): void
+    {
+        echo static::getAttachment($id);
+    }
+
+    /**
+     * Removes doctype from SVG string.
+     *
+     * @param  string $svg SVG markup.
+     * @return string      SVG with removed doctype.
+     */
+    public static function removeDoctype(string $svg): string
+    {
+        return trim((string)preg_replace('/\<(\?xml|(\!DOCTYPE[^\>\[]+(\[[^\]]+)?))+[^>]+\>/', '', $svg));
+    }
+}
