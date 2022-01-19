9fcff5e92f0892b1f6fb239cfa1c8dd1077bac38
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-01-19 16:01:30 +0100

    add bootstrap config

---
+++ config/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package DoW Starter Theme
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+return [
+    /**
+     * General classes instantiated at startup
+     */
+    'classes' => [
+        /**
+         * Features
+         */
+        DoWStarterTheme\Features\SVGSupport::class,
+
+        /**
+         * Shortcodes
+         */
+        DoWStarterTheme\Shortcodes\DateShortcode::class,
+    ],
+
+    /**
+     * Widget classes passed to `register_widget`
+     * @see https://developer.wordpress.org/reference/functions/register_widget/
+     */
+    'widgets' => [
+        DoWStarterTheme\Widgets\SocialLinksWidget::class,
+    ],
+];
