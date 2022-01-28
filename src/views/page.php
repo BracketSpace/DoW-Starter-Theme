276599c706a1ad58c83de8a7558adf856efe9282
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-01-28 12:45:07 +0100

    add view template files

---
+++ src/views/page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @var \DoWStarterTheme\View\View $this
+ */
+
+declare(strict_types=1);
+
+?>
+<h1>Page</h1>
---
+++ src/views/single.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var \DoWStarterTheme\View\View $this
+ */
+
+declare(strict_types=1);
+
+$this->layout('other');
+
+?>
+
+<h1>Single</h1>
+
+<?php $this->partial('example'); ?>
