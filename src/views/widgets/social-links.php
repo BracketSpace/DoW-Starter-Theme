a3459a4dddacc07fdf920c2a77b6f73c68d1c5c7
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-03-04 16:36:47 +0100

    update social links widget

---
+++ src/views/widgets/social-links.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @var \DoWStarterTheme\View\View $this
+ */
+
+declare(strict_types=1);
+
+$items = $this->get('items');
+?>
+
+<?php if (count($items) > 1) : ?>
+	<ul class="social-links-menu">
+		<?php foreach ($items as $item) : ?>
+			<li class="social-links-item">
+				<a class="link" href="<?php echo $item['link']['url']; ?>" target="<?php echo $item['link']['target']; ?>" aria-label="<?php echo $item['link']['title']; ?>">
+					<i class="fab fa-<?php echo esc_attr($item['icon']); ?>"></i>
+				</a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+<?php endif; ?>
