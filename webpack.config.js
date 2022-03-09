8730f34616578abafe6140a0857cba1038d148e3
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-03-09 16:43:47 +0100

    add blocks directory alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,6 @@ const defaultConfig = require('@micropackage/scripts/config/webpack.config');
 const path = require('path');
 
 defaultConfig.resolve.alias.config = path.resolve(__dirname, 'config/');
+defaultConfig.resolve.alias.blocks = path.resolve(__dirname, 'src/blocks/');
 
 module.exports = defaultConfig;
