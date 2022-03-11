261e83d5db004884062e49f7cf3849cbec87dc59
Wojtek Szałkiewicz <wojtek@szalkiewicz.pl>
2022-03-11 16:01:36 +0100

    fix eslint errors

--- .tools/index.js
+++ .tools/index.js
@@ -4,16 +4,16 @@
  * External dependencies
  */
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers'
+import { hideBin } from 'yargs/helpers';
 
 /**
  * Internal dependencies
  */
 import commands from './commands/index.js';
 
-yargs(hideBin(process.argv)).scriptName("tools")
-  .usage('$0 <cmd> [args]')
-  .command(commands)
-  .demandCommand()
-  .help()
-  .argv
+yargs(hideBin(process.argv))
+	.scriptName('tools')
+	.usage('$0 <cmd> [args]')
+	.command(commands)
+	.demandCommand()
+	.help().argv;
