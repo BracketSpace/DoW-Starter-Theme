9f743e97306d5e775c5ebda9695fcbfefe6d3674
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2022-03-16 16:37:18 +0100

    update default layout

--- src/views/layouts/index.php
+++ src/views/layouts/index.php
@@ -3,8 +3,14 @@
 declare(strict_types=1);
 
 use DoWStarterTheme\Helpers\View as v;
+
+v::partial('header');
 ?>
 
-<div class="site-content">
-	<?php v::raw('content'); ?>
+<div class="site-inner container-full">
+	<main class="content">
+		<?php v::raw('content'); ?>
+	</main>
 </div>
+
+<?php v::partial('footer'); ?>
