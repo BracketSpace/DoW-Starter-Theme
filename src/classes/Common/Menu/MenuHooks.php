0ccd7d63bd415758e6f98661ee57d1777ce875d9
Wojtek Szałkiewicz <wojtek@bracketspace.com>
2025-06-27 11:28:04 +0200

    fix(menu): update MenuHooks class

--- src/classes/Common/Menu/MenuHooks.php
+++ src/classes/Common/Menu/MenuHooks.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace DoWStarterTheme\Common\Menu;
 
-use KepnerTregoe\Common\Contracts\Hookable;
-use KepnerTregoe\Common\Helpers\SVG;
-use KepnerTregoe\Deps\Illuminate\Support\Collection;
+use DoWStarterTheme\Common\Contracts\Hookable;
+use DoWStarterTheme\Common\Helpers\SVG;
+use DoWStarterTheme\Deps\Illuminate\Support\Collection;
 use WP_Post;
 
 /**
@@ -14,42 +14,43 @@ use WP_Post;
  */
 class MenuHooks implements Hookable
 {
-   /**
-     * Class constructor.
-     *
-     * @param SVG $svg SVG helper instance.
-     */
+    /**
+      * Class constructor.
+      *
+      * @param SVG $svg SVG helper instance.
+      */
     public function __construct(
         private SVG $svg,
     ) {
     }
 
     /**
-	 * Adds icons to menu items.
-	 *
-	 * @filter wp_nav_menu_objects
-	 *
-	 * @param WP_Post[]            $items Menu items.
-	 * @param array<string, mixed> $args  object of wp_nav_menu() arguments.
-	 * @return WP_Post[] customized menu items with icons.
-	 */
-	public function addIcons(array $items) {
+     * Adds icons to menu items.
+     *
+     * @filter wp_nav_menu_objects
+     *
+     * @param array<WP_Post> $items Menu items.
+     * @return array<WP_Post> customized menu items with icons.
+     */
+    public function addIcons(array $items) {
         return Collection::make($items)
-            ->map(function (WP_Post $item) {
-                $icon = get_field('icon', $item);
+            ->map(
+                function (WP_Post $item) {
+                    $icon = get_field('icon', $item);
 
-                if (!is_numeric($icon)) {
+                    if (! is_numeric($icon)) {
+                        return $item;
+                    }
+
+                    $item->title = sprintf(
+                        '%s%s',
+                        $this->svg->getAttachment($icon),
+                        $item->title
+                    );
+
                     return $item;
                 }
-
-                $item->title = sprintf(
-                    '%s%s',
-                    $this->svg->getAttachment($icon),
-                    $item->title
-                );
-
-                return $item;
-            })
+            )
             ->all();
-	}
+    }
 }
